3d028fedbadfaf144686662585018f846c5c717e
mana jalili <jalilimana@gmail.com>
2024-11-20 22:20:37 +0100

    product model attributes

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        "name",
+        "info",
+        "price",
+        "image",
+    ];
+
+    /**
+     * @var array
+     */
+    protected $casts = [
+        'price' => 'integer',
+    ];
 }
