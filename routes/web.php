6183b9342aef5537c20487b381b53759d84e4f66
mana jalili <jalilimana@gmail.com>
2024-11-25 13:00:50 +0100

    set the routes and wrote index function and view for product

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderItemController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProductController::class, 'index']);
+
+Route::resources([
+    'product' => ProductController::class,
+    'order' => OrderController::class,
+    'order_item' => OrderItemController::class,
+]);
